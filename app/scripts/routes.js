205200c58f1af284211878d500876392ff6ca61d
veetesh <veeteshjain@gmail.com>
2015-02-28 19:08:59 +0530

    angular app basic setup

---
+++ app/scripts/routes.js
@@ -0,0 +1,15 @@
+define([
+    ], function()
+    {
+        return {
+            defaultRoutePath: '/',
+            routes: {
+                '/': {
+                    templateUrl: '/views/home.html',
+                    dependencies: [
+                    'controllers/HomeViewController'
+                    ]
+                }
+            }
+        };
+    });
